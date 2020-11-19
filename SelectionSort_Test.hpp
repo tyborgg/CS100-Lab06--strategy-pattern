584dc444816940189d03a2c41614c019fc2ce4ab
amiddendorp22 <amiddendorp22@gmail.com>
2020-11-19 12:36:05 -0800

    Added SelectionSort test for ListContainer.

--- SelectionSort_Test.hpp
+++ SelectionSort_Test.hpp
@@ -8,6 +8,7 @@
 #include "op.hpp"
 #include "mult.hpp"
 #include "container.hpp"
+#include "listContainer.hpp"
 TEST(SelectionSortTest, ThreeNormalTrees)
 {
 	Base* op1 = new Op(7);
@@ -61,5 +62,31 @@ TEST(SelectionSortTest, ThreeTreesOneNeg)
 	EXPECT_EQ(v1->at(0)->evaluate(),18);
 }
 
+TEST(SelectionSortTest, ListContainerSelectionSortTest)
+{
+        Base* op1 = new Op(3);
+        Base* op2 = new Op(3);
+        Base* TreeA = new Mult(op1,op2);
+
+        Base* op3 = new Op(9);
+        Base* op4 = new Op(2);
+        Base* TreeB = new Mult(op3,op4);
+
+        Base* op5 = new Op(-2);
+        Base* op6 = new Op(2);
+        Base* TreeC = new Mult(op5,op6);
+
+        Sort* s1 = new SelectionSort();
+        ListContainer* v1 = new ListContainer();
+	v1->set_sort_function(s1);
+        v1->add_element(TreeB);
+        v1->add_element(TreeA);
+        v1->add_element(TreeC);
+        v1->sort();
+
+        EXPECT_EQ(v1->at(2)->evaluate(), -4);
+        EXPECT_EQ(v1->at(1)->evaluate(),9);
+        EXPECT_EQ(v1->at(0)->evaluate(),18);
+}
 
 #endif //__SELECTIONSORT_TEST_HPP__
