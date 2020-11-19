ecd6c1069a837bb50dac82319a4124dadeb81d55
amiddendorp22 <amiddendorp22@gmail.com>
2020-11-19 12:21:22 -0800

    Added bubbleSort test for VectorContainer

--- bubble_test.hpp
+++ bubble_test.hpp
@@ -42,4 +42,41 @@ TEST(SortTestSet, BubbleSortTest) {
     EXPECT_EQ(container->at(2)->evaluate(), 28);
 }
 
+
+TEST(SortTestSet, VectorBubbleTest) {
+    Op* seven = new Op(7);
+    Op* four = new Op(4);
+    Mult* TreeA = new Mult(seven, four);
+
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Add* TreeB = new Add(three, two);
+
+    Op* ten = new Op(10);
+    Op* six = new Op(6);
+    Sub* TreeC = new Sub(ten, six);
+
+    Sort* s1 = new BubbleSort();
+    VectorContainer* container = new VectorContainer(s1);
+    container->add_element(TreeA);
+    container->add_element(TreeB);
+    container->add_element(TreeC);
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 28);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+
+    container->set_sort_function(s1);
+    container->sort();
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 4);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 28);
+}
+
+
+
 #endif //__BUBBLE_TEST_HPP__
